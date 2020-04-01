73c52c26a7de7c7b9a2956d519e7f0d3dafa7f15
Reid Ashwill <reidashwill@gmail.com>
2020-04-01 09:55:18 -0700

    add backend logic

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+var leapYear = function(year) {
+  if ((year % 4 === 0) && (year % 100 !== 0)) {
+    return true
+  } else {
+    return false;
+  }
+};
+
+
+$(document).ready(function(){
+  $("form#leap-year").submit(function(event){
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+    
+  });
+ 
+})
